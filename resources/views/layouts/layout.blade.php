b5e6da9be13cde58708b5feda834ba9ed46af22f
ravialdo <ravialdo@yahoo.com>
2020-11-10 00:03:34 +0700

    first commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Belajar Livewire - Techiraid.com</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+
+    <!-- Memanggil Style Livewire -->
+    @livewireStyles
+
+</head>
+<body>
+
+    <div class="container-fluid bg-light">
+        <div class="p-5">
+            <h1>Belajar Livewire Bersama <a href="https://techiraid.com" class="text-decoration-none">Techiraid.com</a></h1>
+            <div class="lead">
+                Belajar Livewire pada Framework Laravel 8
+            </div>
+        </div>
+    </div>
+
+    <!-- Memanggil Script Livewire -->
+    @livewireScripts
+
+</body>
+</html>
